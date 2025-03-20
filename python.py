fc89a667bb619525aaec1b8d16dc6fa2582cc38c
K Roger <rogrkalisa@gmail.com>
2025-03-20 17:17:53 +0200

    Version three

--- python.py
+++ python.py
@@ -1,5 +1,14 @@
 # user_input = input("Enter a string: ")
 # print("Number of characters:", len(user_input))
 
-user_input = input("Enter a string: ").replace(" ","")
-print("Number of characters:", len(user_input))
+# user_input = input("Enter a string: ").replace(" ","")
+# print("Number of characters:", len(user_input))
+
+
+words=[]
+for i in range(5):
+     word = input(f"Enter word {i+1}: ")
+     words.append(word)
+     print("\nWord Lengths:")
+for word in words:
+     print(f"'{word}' is {len(word)} characters")
