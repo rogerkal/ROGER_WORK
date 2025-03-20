698a251b965c033696e525b6bf1803cebb12c707
K Roger <rogrkalisa@gmail.com>
2025-03-20 17:15:35 +0200

    version two

--- python.py
+++ python.py
@@ -1,2 +1,5 @@
-user_input = input("Enter a string: ")
+# user_input = input("Enter a string: ")
+# print("Number of characters:", len(user_input))
+
+user_input = input("Enter a string: ").replace(" ","")
 print("Number of characters:", len(user_input))
